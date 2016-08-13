c21c52b9a2c165a9d74dc9ca012feb26a4fe4a8a
Eivind Røe <eivindroe@gmail.com>
2016-08-14 01:27:40 +0200

    Refactored application javascript
    Organized functions so it follows the same pattern all the way through and easier to extend.
    Use strict mode to force all variables to be declared

--- js/app.js
+++ js/app.js
@@ -1,71 +1,119 @@
 /**
  * Created by Eivind Røe <eivindroe@gmail.com> on 06.08.2016.
  */
-var Map = (function() {
-    var TentMap = L.map("map-tent-sites").setView([63.412222, 10.404722], 4),
-        locationCircle;
+(function($) {
+    'use strict';
 
-    // Configure layers
-    var WorldImagery = L.tileLayer(
-        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, '+
-            'GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-        }),
-        OpenStreetMap = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            subdomains: ["a","b","c"]
-        });
-
-    // Configure base maps
-    var baseMaps = {
-        "World imagery": WorldImagery,
-        "Open street map" : OpenStreetMap
-    };
-
-    // Define main layer
-    OpenStreetMap.addTo(TentMap);
-
-    // Define available layers
-    L.control.layers(baseMaps).addTo(TentMap);
-
-    // Add view position button
-    L.easyButton({
-        position: "topleft",
-        states: [{
-            icon: "fa-crosshairs",
-            title: "View my position",
-            onClick: function(button, map) {
-                map.locate();
-            }
-        }]
-    }).addTo(TentMap);
-
-    TentMap.on("locationfound", function(event) {
-        if(locationCircle) {
-            TentMap.removeLayer(locationCircle);
+    function getTime() {
+        if(typeof Date.now !== typeof undefined) {
+            return Date.now();
+        } else {
+            // Fallback for IE8
+            return new Date().getTime();
         }
-        TentMap.setView(event.latlng, 10);
-        locationCircle = L.circle(event.latlng, event.accuracy, {
-            color: "red",
-            fillColor: "#f03",
-            fillOpacity: 0.5
-        }).addTo(TentMap);
-    });
+    }
 
-    TentMap.on("locationerror", function(event) {
-        alert("Could not find your location. Please turn on gps and try again");
-        console.log(event.message);
-    });
+    var Sites = (function() {
+        var fncCallbackOnFetchedSites,
+            strSiteApiUrl = "http://api.mytent.site/tentsites";
 
-    return {
-        "initialize" : function() {
-            return this;
-        },
-        "placeSites" : function(tentSites) {
-            var photoLayer = L.photo.cluster().on('click', function (evt) {
+        function hasExtendedCacheLifeTime() {
+            var intLastFetchTime = localStorage.getItem("Sites.lastFetchTime");
+            if(!intLastFetchTime) {
+                return true;
+            } else {
+                // Cache lifetime is one hour
+                return ((getTime() - intLastFetchTime) > 3600000);
+            }
+        }
+
+        function fetchSites() {
+            if(hasExtendedCacheLifeTime()) {
+                localStorage.removeItem("Sites.all");
+            }
+            if(localStorage.getItem("Sites.all")) {
+                fncCallbackOnFetchedSites(JSON.parse(localStorage.getItem("Sites.all")));
+            } else {
+                $.ajax({
+                    url: strSiteApiUrl,
+                    success: function(response) {
+                        var tentSites = [];
+                        if(parseInt(response.code) === 200 && typeof response.data !== typeof undefined) {
+                            $.each(response.data, function(key, photo) {
+                                tentSites.push({
+                                    id: photo["id"],
+                                    reported_by: photo["reported_by"],
+                                    lat: photo["latitude"],
+                                    lng: photo["longitude"],
+                                    location_name: photo["location_name"],
+                                    created_time: photo["created_time"],
+                                    likes: photo["likes"],
+                                    img_location: photo["img_location"],
+                                    external_id: photo["external_id"],
+                                    thumbnail: photo["thumbnail_location"],
+                                    caption: photo["caption"],
+                                    created_at: photo["created_at"],
+                                    updated_at: photo["updated_at"]
+                                });
+                            });
+                        }
+                        localStorage.setItem("Sites.lastFetchTime", getTime());
+                        localStorage.setItem("Sites.all", JSON.stringify(tentSites));
+                        fncCallbackOnFetchedSites(tentSites);
+                    }, error: function(error) {
+                        console.log(error);
+                    }
+                });
+            }
+        }
+
+        return {
+            "onFetchedSites" : function(fncCallback) {
+                fncCallbackOnFetchedSites = fncCallback;
+                fetchSites();
+            }
+        }
+    })();
+
+    function Map() {
+        var TentMap,
+            locationCircle,
+            $map;
+
+        // Configure layers
+        var WorldImagery = L.tileLayer(
+            'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+                attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, '+
+                'GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+            }),
+            OpenStreetMap = L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                subdomains: ["a","b","c"]
+            });
+
+        // Configure base maps
+        var baseMaps = {
+            "World imagery": WorldImagery,
+            "Open street map" : OpenStreetMap
+        };
+
+        function bindMap() {
+            TentMap = L.map("map-tent-sites").setView([63.412222, 10.404722], 4);
+        }
+
+        function addLayerToMap(layer) {
+            layer.addTo(TentMap);
+        }
+
+        function addLayersToMap(layers) {
+            L.control.layers(layers).addTo(TentMap);
+        }
+
+        function placeSites(tentSites) {
+            var photoLayer = L.photo.cluster().on("click", function (evt) {
                 var photo = evt.layer.photo,
                     template = '<img src="{img_location}" width="300" height="300" /></a><p>{caption}</p>';
-                if (photo.video &&  (!!document.createElement('video').canPlayType("video/mp4; codecs=avc1.42E01E,mp4a.40.2"))) {
+                if (photo.video && (!!document.createElement("video").canPlayType("video/mp4; codecs=avc1.42E01E,mp4a.40.2"))) {
                     template = '<video autoplay controls poster="{img_location}" width="300" height="300">' +
                         '<source src="{video}" type="video/mp4"/></video>';
                 }
@@ -77,80 +125,51 @@ var Map = (function() {
             photoLayer.add(tentSites).addTo(TentMap);
             //TentMap.fitBounds(photoLayer.getBounds());
         }
-    }
-})(jQuery);
 
-var Sites = (function($) {
-    var fncCallbackOnFetchedSites,
-        strSiteApiUrl = "http://api.mytent.site/tentsites";
+        return {
+            initialize: function() {
+                bindMap();
+                addLayerToMap(OpenStreetMap);
+                addLayersToMap(baseMaps);
 
-    function hasExtendedCacheLifeTime() {
-        var intLastFetchTime = localStorage.getItem("Sites.lastFetchTime");
-        if(!intLastFetchTime) {
-            return true;
-        } else {
-            // Cache lifetime is one hour
-            return ((getTime() - intLastFetchTime) > 3600000);
-        }
-    }
+                Sites.onFetchedSites(function(sites) {
+                    placeSites(sites);
+                });
 
-    function fetchSites() {
-        if(hasExtendedCacheLifeTime()) {
-            localStorage.removeItem("Sites.all");
-        }
-        if(localStorage.getItem("Sites.all")) {
-            fncCallbackOnFetchedSites(JSON.parse(localStorage.getItem("Sites.all")));
-        } else {
-            $.ajax({
-                url: strSiteApiUrl,
-                success: function(response) {
-                    var tentSites = [];
-                    if(parseInt(response.code) === 200 && typeof response.data !== typeof undefined) {
-                        $.each(response.data, function(key, photo) {
-                            tentSites.push({
-                                id: photo["id"],
-                                reported_by: photo["reported_by"],
-                                lat: photo["latitude"],
-                                lng: photo["longitude"],
-                                location_name: photo["location_name"],
-                                created_time: photo["created_time"],
-                                likes: photo["likes"],
-                                img_location: photo["img_location"],
-                                external_id: photo["external_id"],
-                                thumbnail: photo["thumbnail_location"],
-                                caption: photo["caption"],
-                                created_at: photo["created_at"],
-                                updated_at: photo["updated_at"]
-                            });
-                        });
+                $map = $("#map-tent-sites");
+
+                // Add view position button
+                L.easyButton({
+                    position: "topleft",
+                    states: [{
+                        icon: "fa-crosshairs",
+                        title: "View my position",
+                        onClick: function(button, map) {
+                            map.locate();
+                        }
+                    }]
+                }).addTo(TentMap);
+
+                TentMap.on("locationfound", function(event) {
+                    if(locationCircle) {
+                        TentMap.removeLayer(locationCircle);
                     }
-                    localStorage.setItem("Sites.lastFetchTime", getTime());
-                    localStorage.setItem("Sites.all", JSON.stringify(tentSites));
-                    fncCallbackOnFetchedSites(tentSites);
-                }, error: function(error) {
-                    console.log(error);
-                }
-            });
+                    TentMap.setView(event.latlng, 10);
+                    locationCircle = L.circle(event.latlng, event.accuracy, {
+                        color: "red",
+                        fillColor: "#f03",
+                        fillOpacity: 0.5
+                    }).addTo(TentMap);
+                });
+
+                TentMap.on("locationerror", function(event) {
+                    alert("Could not find your location. Please turn on gps and try again");
+                    console.log(event.message);
+                });
+            }
         }
     }
 
-    return {
-        "onFetchedSites" : function(fncCallback) {
-            fncCallbackOnFetchedSites = fncCallback;
-            fetchSites();
-        }
-    }
+    Map = new Map();
+    Map.initialize();
 })(jQuery);
-
-function getTime() {
-    if(typeof Date.now !== typeof undefined) {
-        return Date.now();
-    } else {
-        // Fallback for IE8
-        return new Date().getTime();
-    }
-}
-
-(function() {
-    Sites.onFetchedSites(function (sites) { Map.placeSites(sites) });
-})();
